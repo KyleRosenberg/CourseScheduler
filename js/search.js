337d7942237c3281931ed689a89dfad7d635152a
KyleRosenberg <kyletherosenberg@icloud.com>
2018-12-27 14:05:47 -0700

    Add Classes to Saved List

--- js/search.js
+++ js/search.js
@@ -1,6 +1,8 @@
 class_list = []
 srcdb = ''
 
+saved_classes = {}
+
 $(document).ready(function(event) {
     $('input[name=srcdb]').val('2191');
     $('#submit_search').click(function(){
@@ -110,43 +112,147 @@ function view_sections(i){
             'srcdb':srcdb
         },
         success: function(data){
-            $('#popup_temp').removeClass('active');
-            data = JSON.parse(data)
-            popup_html = `<a class="ui black right ribbon label">Last Updated: ${data.last_updated}</a><br/>`
-            popup_html += `<strong>Credit Hours:</strong> ${data.hours_text}<br/>`;
-            if (data.seats=="Varies by section")
-                popup_html += "<strong>Max Seats:</strong>"
-            popup_html += `${data.seats}<br/>`
-            if (data.restrict_info!="")
-                popup_html += `<span class="info_head">Registration Restrictions</span> <p>${data.restrict_info}</p>`
-            if (data.clssnotes!="")
-                popup_html += `<span class="info_head">Class Notes</span> <p>${data.clssnotes}</p>`
-            popup_html += `<span class="info_head">Course Description</span> <p>${data.description}</p>`
-            bad_section_element = $.parseHTML(data.all_sections);
-            headers = $($(bad_section_element).children()[0]).children();
-            table = '<table class="ui celled table"><thead><tr><th>Saved</th>'
-            for (j = 0; j<headers.length; j++){
-                table += `<th>${headers[j].innerText}</th>`;
-            }
-            table += '</tr></thead>'
-            rows = $(bad_section_element).children().slice(1)
-            for (r = 0; r<rows.length; r++){
-                console.log(rows[r])
-                cells = rows[r].children
-                id = cells[0].innerText
-                id = id.substring(id.indexOf(':')+1)
-                table += `<tr><td><div class="ui checkbox"><input type="checkbox" name="${id}"><label></label></div></td>`
-                for (c = 0; c<cells.length; c++){
-                    txt = cells[c].innerText
-                    txt = txt.substring(txt.indexOf(':')+1)
-                    table += `<td>${txt}</td>`;
-                }
-                table += '</tr>'
-            }
-            table += '</table>'
-            popup_html += table
-            $('.ui.popup').append(popup_html);
-            $('#popup_temp').removeClass('active');
+            showDetails(data);
         }
     });
+}
+
+function showDetails(data){
+    $('.ui.popup').children().remove();
+    $('#popup_temp').removeClass('active');
+    data = JSON.parse(data)
+    popup_html = `<h3>${data.code} - ${data.title}</h3><div id="popup_temp" class="ui active text loader">Getting Details...</div>`;
+    popup_html += `<a class="ui black left ribbon label">Last Updated: ${data.last_updated}</a><br/>`
+    popup_html += `<strong>Credit Hours:</strong> <span>${data.hours_text}<span><br/>`;
+    if (data.seats=="Varies by section")
+        popup_html += "<strong>Max Seats:</strong>"
+    popup_html += `<span>${data.seats}</span><br/>`
+    if (data.restrict_info!="")
+        popup_html += `<span class="info_head">Registration Restrictions</span> <p>${data.restrict_info}</p>`
+    if (data.clssnotes!="")
+        popup_html += `<span class="info_head">Class Notes</span> <p>${data.clssnotes}</p>`
+    popup_html += `<span class="info_head">Course Description</span> <p>${data.description}</p>`
+    popup_html += `<span class="info_head">Schedule and Location</span> <p>${data.meeting_html}</p>`;
+    popup_html += `<span class="info_head">Instructors</span> <p>${data.instructordetail_html}</p>`;
+    bad_section_element = $.parseHTML(data.all_sections);
+    headers = $($(bad_section_element).children()[0]).children();
+    table = '<table class="ui selectable celled table"><thead><tr><th>Saved</th>'
+    for (j = 0; j<headers.length; j++){
+        table += `<th>${headers[j].innerText}</th>`;
+    }
+    table += '</tr></thead>'
+    rows = $(bad_section_element).children().slice(1)
+    for (r = 0; r<rows.length; r++){
+        cells = rows[r].children
+        id = cells[0].innerText
+        id = id.substring(id.indexOf(':')+1).trim()
+        row = `<tr onclick="selectCourseRow(this)"><td><div class="ui checkbox"><input type="checkbox" name="${id}" `;
+        if (class_saved(id)){
+            row += 'checked';
+        }
+        row += `><label></label></div></td>`
+        for (c = 0; c<cells.length; c++){
+            txt = cells[c].innerText
+            txt = txt.substring(txt.indexOf(':')+1)
+            row += `<td>${txt}</td>`;
+        }
+        row += '</tr>'
+        table += row
+    }
+    table += '</table>'
+    popup_html += table
+    $('.ui.popup').append(popup_html);
+    $('.ui.popup input[type="checkbox"]').click(function(event){
+        toggleSaveCourse($(this));
+        event.stopPropagation();
+    })
+    $('#popup_temp').removeClass('active');
+}
+
+function class_saved(id){
+    keys = Object.keys(saved_classes);
+    for (i = 0; i<keys.length; i++){
+        if (keys[i]==id.toString()) return true;
+    }
+    return false;
+}
+
+function selectCourseRow(course){
+    code = $('.ui.popup h3')[0].innerText.slice(0, 9);
+    crn = course[0].name.trim();
+    $.ajax({
+        type:'POST',
+        url:'/search',
+        data:{
+            'type':'view_sections',
+            'group':`code:${code}`,
+            'key':`crn:${crn}`,
+            'matched':`crn:${crn}`,
+            'srcdb':srcdb
+        },
+        success: function(data){
+            showDetails(data);
+        }
+    })
+}
+
+function toggleSaveCourse(course){
+    code = $('.ui.popup h3')[0].innerText.slice(0, 9);
+    crn = course[0].name.trim();
+    if (course[0].checked){
+        $.ajax({
+            type:'POST',
+            url:'/search',
+            data:{
+                'type':'view_sections',
+                'group':`code:${code}`,
+                'key':`crn:${crn}`,
+                'matched':`crn:${crn}`,
+                'srcdb':srcdb
+            },
+            success: function(data){
+                saved_classes[crn] = data;
+                updateCourseList()
+            }
+        });
+    } else {
+        delete saved_classes[crn];
+        updateCourseList()
+    }
+}
+
+function updateCourseList(){
+    dis = $('#save_display');
+    dis.children().remove()
+    var sorted = [];
+    for(var key in saved_classes) {
+        sorted[sorted.length] = key;
+    }
+    sorted.sort();
+    for (var s in sorted){
+        v = JSON.parse(saved_classes[sorted[s]]);
+        code = v.code;
+        if (v.hours_text.includes('Lec')){
+            code += ' - LEC';
+        }
+        else if (v.hours_text.includes('Rec')){
+            code += ' - REC';
+        }
+        else if (v.hours_text.includes('Lab')){
+            code += ' - LAB';
+        }
+        else if (v.hours_text.includes('Sem')){
+            code += ' - SEM';
+        }
+        else if (v.hours_text.includes('Pra')){
+            code += ' - PRA';
+        }
+        ele = $(v.meeting_html)[0]
+        ele = ele.innerText
+        time = ele.substring(0, ele.indexOf(' in'));
+        html = `<div class="item"><div class="right floated content"><div class="ui mini button" style="padding:10px;">
+            Toggle</div></div><div id="course_item" class="ui label" style="padding:1px;padding-top:10px;">${code}
+            <div class="detail">${time}</div></div></div>`;
+        dis.append(html);
+    }
 }
