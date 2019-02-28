0320fc81c267b8ff646783fbed852af8a1ca7661
Kyle <kyletherosenberg@icloud.com>
2019-02-27 21:07:35 -0700

    Revert "Complete Sprint"

--- js/init.js
+++ js/init.js
@@ -13,7 +13,6 @@ $(document).ready(function() {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        showError(error.message);
     });
     firebase.auth().onAuthStateChanged(function(user) {
        if (user) {
@@ -33,20 +32,9 @@ function handleLogin(){
             // Sign-out successful.
             $('#login').text("Log In");
             $('#name').text('');
-
-            window.sessionStorage.removeItem('token')
-            window.sessionStorage.removeItem('dict')
-            window.sessionStorage.removeItem('updated_cart')
-            for (k in calendar_classes){
-                removeClassFromCalendar(calendar_classes[k]);
-            }
-            saved_classes = {}
-            calendar_classes = {}
-            updateCourseList();
-            proc_keyword_data('Unauthorized');
         })
         .catch(function(error) {
-            showError(error.message);
+        // An error happened
         });
     } else {
         firebase.auth().signInWithPopup(provider).then(function(result) {
@@ -67,27 +55,8 @@ function handleLogin(){
           var email = error.email;
           // The firebase.auth.AuthCredential type that was used.
           var credential = error.credential;
-          showError(error.message);
+          alert('Failed to log in.');
           // ...
         });
     }
-}
-
-function showError(message){
-    error = $.parseHTML(`<div class="ui transition hidden error message">
-        <div class="header">
-            Something went wrong...
-        </div>
-        ${message}
-    </div>`);
-    $('body').append(error)
-    $(error).transition({
-        animation: 'vertical flip in'
-    }).transition({
-        animation: 'vertical flip out',
-        interval: 5000,
-        onComplete: function(){
-            $(error).remove();
-        }
-    })
 }
